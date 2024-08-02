efb2910c3a5dd74ba3821453fc19bb263a13173e
Koval Yaroslav <yaroslav@koval.page>
2024-08-02 15:48:36 +0200

    Update default value ALLOWED_HOSTS

--- app/utils/config.py
+++ app/utils/config.py
@@ -6,7 +6,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    ALLOWED_HOSTS: str = 'localhost,127.0.0.1'
+    ALLOWED_HOSTS: str = 'localhost,127.0.0.1,*.trycloudflare.com'
     CRYPT_KEY: str = 'fl5JcIwHh0SM87Vl18B_Sn65lVOwhYIQ3fnfGYqpVlE='
     SECRET_KEY: str = 'devsecretkey'
     DISABLE_DOCS: int = 0
