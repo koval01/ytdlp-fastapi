373661976b07feb0d099edaecb95093c9b1c6d11
Koval Yaroslav <yaroslav@koval.page>
2024-08-07 10:15:47 +0200

    Add manifest route

--- app/routes/v1/__init__.py
+++ app/routes/v1/__init__.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from . import video, playback
+from . import video, playback, mfest
 
 router = APIRouter(prefix="/v1")
 
 router.include_router(video.router)
 router.include_router(playback.router)
+router.include_router(mfest.router)
---
+++ app/routes/v1/mfest/__init__.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+# from . import mpd
+
+router = APIRouter(prefix="/manifest")
+
+# router.include_router(mpd.router)
