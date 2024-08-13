3fb1853ff9d9c7ed5a585c3236c311ae9f80d486
Koval Yaroslav <yaroslav@koval.page>
2024-08-13 16:43:31 +0200

    Update utils

--- app/utils/hls.py
+++ app/utils/hls.py
@@ -1,29 +1,54 @@
 import m3u8
 from fastapi import Request
 
-from app.utils.crypto import Cryptography
 from app.utils.config import settings
+from app.utils.crypto import Cryptography
 
 
 class HLSReplacer:
-    @staticmethod
-    def replace_manifest_links(manifest_content, request: Request) -> str:
-        playlist = m3u8.loads(manifest_content)
-        cryptography = Cryptography()
+    """
+    This class provides functionality to replace HLS manifest URLs with encrypted URLs
+    that point to local endpoints, enhancing security and controlling access.
+    """
 
+    @staticmethod
+    def replace_manifest_links(manifest_content: str, request: Request) -> str:
+        """
+        Replaces the URLs in an HLS playlist manifest with encrypted local URLs.
+
+        Args:
+            manifest_content (str): The original HLS manifest content as a string.
+            request (Request): The FastAPI request object.
+
+        Returns:
+            str: The updated HLS manifest content with replaced URLs.
+        """
+        playlist = m3u8.loads(manifest_content)  # Parse the manifest content
+        cryptography = Cryptography()  # Initialize the Cryptography utility
+
+        # Determine the client host for encryption
         client_host = request.client.host
         x_host = request.headers.get("X-Client-Host")
         if settings.REST_MODE and x_host:
             client_host = x_host
 
-        # Function to replace URLs in the playlist
-        def replace_url(url):
-            # Construct the new URL based on the type
+        def replace_url(url: str) -> str:
+            """
+            Constructs and returns a new encrypted URL for a given HLS segment or playlist.
+
+            Args:
+                url (str): The original URL to be replaced.
+
+            Returns:
+                str: The new encrypted URL.
+            """
             _host = f"{request.url.scheme}://{request.url.netloc}"
             _data = cryptography.encrypt_json({
                 'url': url,
                 'client_host': client_host
             })
+
+            # Determine the new URL based on the type (playlist or segment)
             if 'hls_playlist' in url:
                 new_url = f"{_host}/v1/manifest/hls/{_data}.m3u8"
             else:
@@ -31,44 +56,52 @@ class HLSReplacer:
 
             return new_url
 
-        # Replace URLs in segments and key URLs
+        # Replace URLs in segments and associated keys
         if playlist.segments:
             for segment in playlist.segments:
-                segment.uri = replace_url(segment.uri)
+                segment.uri = replace_url(segment.uri)  # Replace segment URL
 
+                # Replace key URL if present
                 if segment.key and segment.key.uri:
                     segment.key.uri = replace_url(segment.key.uri)
 
+                # Replace initialization section URL if present
                 if segment.init_section and segment.init_section.uri:
                     segment.init_section.uri = replace_url(segment.init_section.uri)
 
-        # Replace URLs in media
+        # Replace URLs in media segments
         if playlist.media:
             for media in playlist.media:
                 media.uri = replace_url(media.uri)
 
-        # Optimize resolution groups replacement
+        # Optimize and replace URLs in resolution groups
         resolution_groups = {}
         i = 0
         while i < len(playlist.playlists):
             p = playlist.playlists[i]
+
+            # Filter out non-MP4 (e.g., VP9) streams
             if 'vp09' not in p.stream_info.codecs:
                 del playlist.playlists[i]
                 continue  # Skip incrementing i to check the next item at the same index
+
             resolution = p.stream_info.resolution
             if resolution not in resolution_groups:
+                # Add a new resolution group if it doesn't exist
                 resolution_groups[resolution] = p
                 p.uri = replace_url(p.uri)
                 i += 1
             else:
                 existing_playlist = resolution_groups[resolution]
                 if p.stream_info.bandwidth > existing_playlist.stream_info.bandwidth:
+                    # Replace the existing playlist with a higher bandwidth one
                     resolution_groups[resolution] = p
                     p.uri = replace_url(p.uri)
                     del playlist.playlists[i]
                 else:
+                    # Remove the current playlist if a better one already exists
                     del playlist.playlists[i]
 
-        # Return the updated manifest content
+        # Return the updated manifest content as a string
         updated_manifest = playlist.dumps()
         return updated_manifest
--- app/utils/url_replacer.py
+++ app/utils/url_replacer.py
@@ -9,68 +9,91 @@ from app.utils.crypto import Cryptography
 
 class URLValidator:
     """
-    This class provides functionality to validate and replace specific video playback URLs in a given data structure
-    with encrypted URLs pointing to a local playback endpoint.
+    This class provides functionality to validate and replace specific video playback URLs
+    in a given data structure with encrypted URLs pointing to a local playback endpoint.
     """
 
     def __init__(self, request: Request) -> None:
+        """
+        Initialize the URLValidator with the request object and compile regex patterns.
+
+        Args:
+            request (Request): The FastAPI request object.
+        """
         self.request = request
+
+        # Regex pattern to match video playback URLs
         self.url_pattern_playback = re.compile(
             r"https://rr(?P<host>[^/]+)\.(?:googlevideo|c\.youtube)\.com/videoplayback\?(?P<query>.+)"
         )
+
+        # Regex pattern to match manifest URLs
         self.url_pattern_manifest = re.compile(
             r"https://(?:manifest\.googlevideo\.com|www\.youtube\.com)"
             r"/api/manifest/hls_(?:variant|playlist)/(?P<query>.+)"
         )
-        self.crypto = Cryptography()
+
+        self.crypto = Cryptography()  # Initialize the Cryptography utility
 
     def _replace_url(self, url: str) -> str:
         """
-        Replaces a single video playback URL with an encrypted local URL if it matches the pattern.
+        Replaces a single video playback or manifest URL with an encrypted local URL if it matches the pattern.
 
         Args:
-            url: The video playback URL to replace.
+            url (str): The video playback or manifest URL to replace.
 
         Returns:
-            The encrypted local URL if the input URL matches the pattern, otherwise returns the original URL.
+            str: The encrypted local URL if the input URL matches the pattern, otherwise the original URL.
         """
+        # Match video playback URL pattern
         match_playback = self.url_pattern_playback.search(url)
 
+        # Determine the client host for encryption
         client_host = self.request.client.host
         x_host = self.request.headers.get("X-Client-Host")
         if settings.REST_MODE and x_host:
             client_host = x_host
 
+        # Replace video playback URL if matched
         if match_playback:
-            _data = self.crypto.encrypt_json({
+            encrypted_data = self.crypto.encrypt_json({
                 'url': url,
                 'client_host': client_host
             })
-            return f"{self.request.url.scheme}://{self.request.url.netloc}/v1/playback/{_data}"
+            return f"{self.request.url.scheme}://{self.request.url.netloc}/v1/playback/{encrypted_data}"
 
+        # Match manifest URL pattern
         match_manifest = self.url_pattern_manifest.search(url)
+
+        # Replace manifest URL if matched
         if match_manifest:
-            _data = self.crypto.encrypt_json({
+            encrypted_data = self.crypto.encrypt_json({
                 'url': url,
                 'client_host': client_host
             })
-            return f"{self.request.url.scheme}://{self.request.url.netloc}/v1/manifest/hls/{_data}"
+            return f"{self.request.url.scheme}://{self.request.url.netloc}/v1/manifest/hls/{encrypted_data}"
 
+        # Return the original URL if no pattern matched
         return url
 
     def _process_data(self, data: dict | list) -> None:
         """
-        Recursively searches through the data structure and replaces video playback URLs if they match the pattern.
+        Recursively searches through the data structure and replaces URLs if they match the pattern.
 
         Args:
-            data: The data structure to process. Can be a dictionary or a list.
+            data (dict | list): The data structure to process. Can be a dictionary or a list.
         """
         if isinstance(data, dict):
+            # Iterate through dictionary items
             for key, value in data.items():
                 if isinstance(value, str):
+                    # Replace URL if value is a string
                     data[key] = self._replace_url(value)
                 elif isinstance(value, (dict, list)):
+                    # Recursively process nested dictionaries or lists
                     self._process_data(value)
+
+            # Special handling for 'formats' key in the data
             if 'formats' in data:
                 filtered_formats = [
                     item for item in data['formats']
@@ -81,12 +104,17 @@ class URLValidator:
                     manifest_url = last_format.get('manifest_url')
                     if manifest_url:
                         data['manifest_url'] = self._replace_url(manifest_url)
+                # Remove 'formats' key from the data after processing
                 del data['formats']
+
         elif isinstance(data, list):
+            # Iterate through list items
             for i, item in enumerate(data):
                 if isinstance(item, str):
+                    # Replace URL if item is a string
                     data[i] = self._replace_url(item)
                 elif isinstance(item, (dict, list)):
+                    # Recursively process nested dictionaries or lists
                     self._process_data(item)
 
     def replace_urls(self, data: dict | list) -> YouTubeResponse:
@@ -95,10 +123,12 @@ class URLValidator:
         if they match the pattern.
 
         Args:
-            data: The data structure to process. Can be a dictionary or a list.
+            data (dict | list): The data structure to process. Can be a dictionary or a list.
 
         Returns:
-            The data structure with the URLs replaced if matched.
+            YouTubeResponse: The data structure with the URLs replaced if matched.
         """
+        # Process the data to replace URLs
         self._process_data(data)
+        # Return the processed data as a YouTubeResponse object
         return YouTubeResponse(**data)
